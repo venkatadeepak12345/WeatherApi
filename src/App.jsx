6e623ff1fc2fa25c9375254698a29451f542bac4
Hexagon Digital Services <hexagonsservices@gmail.com>
2025-03-13 13:50:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div>Lets Create Weather-App using OpenWeatherAPI</div>
+
+     const API_KEY = 'YOUR KEY SEE VIDEO TO GET IT';
+
+    // `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}` //API GEO LOCATION API
+    // `https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${API_KEY}&units=metric` // API CURRENT WEATHER
   )
 }
 
